4e200fdf7b552e002173d5c43cafe79e8b157a8f
s6108 <s6108s6108@gmail.com>
2026-07-02 16:02:34 +0800

    v2

---
+++ models.py
@@ -0,0 +1,42 @@
+from config import get_key
+
+
+ZHIPU_API_KEY = get_key("ZHIPU_API_KEY")
+DEEPSEEK_API_KEY = get_key("DEEPSEEK_API_KEY")
+KIMI_API_KEY = get_key("KIMI_API_KEY")
+DOUBAO_API_KEY = get_key("DOUBAO_API_KEY")
+DASHSCOPE_API_KEY = get_key("DASHSCOPE_API_KEY")
+
+
+model_options = {
+    "DeepSeek": {
+        "base_url": "https://api.deepseek.com",
+        "model": "deepseek-chat",
+        "key": DEEPSEEK_API_KEY,
+    },
+    "GLM-4V": {
+        "base_url": "https://open.bigmodel.cn/api/paas/v4/",
+        "model": "glm-4v-plus",
+        "key": ZHIPU_API_KEY,
+    },
+    "GLM-4": {
+        "base_url": "https://open.bigmodel.cn/api/paas/v4/",
+        "model": "glm-4-plus",
+        "key": ZHIPU_API_KEY,
+    },
+    "Kimi": {
+        "base_url": "https://api.moonshot.cn/v1",
+        "model": "moonshot-v1-8k",
+        "key": KIMI_API_KEY,
+    },
+    "Doubao-Pro": {
+        "base_url": "https://ark.cn-beijing.volces.com/api/v3",
+        "model": "ep-20260415022601-jm5b7",
+        "key": DOUBAO_API_KEY,
+    },
+    "Qwen": {
+        "base_url": "https://dashscope.aliyuncs.com/compatible-mode/v1",
+        "model": "qwen-plus",
+        "key": DASHSCOPE_API_KEY,
+    },
+}
