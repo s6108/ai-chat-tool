df8aa49025a8c02e53cce217370b6267271abf22
s6108 <s6108s6108@gmail.com>
2026-07-07 00:40:37 +0800

    第三次修改登錄

--- app.py
+++ app.py
@@ -531,8 +531,8 @@ if not st.session_state.user:
                     plan = get_user_plan(res.user.id)
                     save_device_session(res.user, res.session, plan)
 
-                st.success("登录成功！")
-                st.rerun()
+                st.success("登录成功！请手动刷新一次页面。")
+                st.stop()
 
             except Exception as e:
                 st.error(f"登录失败: {e}")
