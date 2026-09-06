e431b8bbcdb6a0a9124ca850cd4ad6bc5a670324
s6108 <s6108s6108@gmail.com>
2026-09-07 00:50:37 +0800

    Megor调节侧边栏导航条

--- app.py
+++ app.py
@@ -1958,6 +1958,7 @@ st.markdown(
                     height: 48px !important;
                     min-width: 48px !important;
                     min-height: 48px !important;
+                    left: 8px !important;
                 }
 
                 [data-testid="stSidebarCollapsedControl"] button {
